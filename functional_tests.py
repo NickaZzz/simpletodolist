586a2d3ba7b869c4f0cf2a2b81a96944e08aafe4
Azanov Nikita <azanov.nik74@gmail.com>
2020-01-29 12:16:58 +0700

    Check the ability to enter a list item in FT

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 
@@ -17,13 +19,31 @@ class NewVisitorTest(unittest.TestCase):
 
         # Она видит, что заголовок и шапка страницы говорят о списках
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn("To-Do", header_text)
 
-        self.fail('Закончить тест!')
         # Ей сразу же предлагается ввести элемент списка
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
         # Она набирает в текстовом поле "Купить павлиньи перья"
+        inputbox.send_keys('Купить павлиньи перья')
+
         # Когда она нажимает enter, страница обновляется, и теперь страница содержит
         # "1: Купить павлиньи перья" в качестве элемента списка
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Купить павлиньи перья' for row in rows)
+        )
+
+        self.fail('Закончить тест!')
         # Текстовое поле по-прежнему приглашает её добавить ещё один элемент.
         # Она вводит "Сделать мушку из павлиньих перьев"
         # Страница снова обновляется, и теперь показывает оба элемента её списка
