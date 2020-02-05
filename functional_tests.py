f5764228f4c8401e336ab6e1d47f8d84c59f6de3
Azanov Nikita <azanov.nik74@gmail.com>
2020-02-05 16:29:13 +0700

    Can processes one list item

--- functional_tests.py
+++ functional_tests.py
@@ -39,15 +39,25 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Купить павлиньи перья' for row in rows),
-            "Новый элемент списка не появился в таблице"
-        )
+        self.assertIn('1: Купить павлиньи перья', [row.text for row in rows])
 
-        self.fail('Закончить тест!')
         # Текстовое поле по-прежнему приглашает её добавить ещё один элемент.
         # Она вводит "Сделать мушку из павлиньих перьев"
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys("Сделать мушку из павлиньих перьев")
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
         # Страница снова обновляется, и теперь показывает оба элемента её списка
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Купить павлиньи перья', [row.text for row in rows])
+        self.assertIn(
+            "2: Сделать мушку из павлиньих перьев",
+            [row.text for row in rows]
+        )
+
+
+        self.fail('Закончить тест!')
         # Эдит интересно, запомнит ли сайт её список. Далее она видит, что
         # сайт сгенерировал для неё уникальный URL-адрес - об этом
         # выводится небольшой текст с объяснениями.
