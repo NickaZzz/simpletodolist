4e0cde762fe209fa3c4d938bfd735607d9bd0f7f
Azanov Nikita <azanov.nik74@gmail.com>
2020-02-11 19:26:46 +0700

    Rename all id and name in input items. Does not work

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -16,7 +16,7 @@ class NewVisitorTest(FunctionalTest):
         self.assertIn("To-Do", header_text)
 
         # Ей сразу же предлагается ввести элемент списка
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box()
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
@@ -33,7 +33,7 @@ class NewVisitorTest(FunctionalTest):
 
         # Текстовое поле по-прежнему приглашает её добавить ещё один элемент.
         # Она вводит "Сделать мушку из павлиньих перьев"
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box()
         inputbox.send_keys("Сделать мушку из павлиньих перьев")
         inputbox.send_keys(Keys.ENTER)
         # Страница снова обновляется, и теперь показывает оба элемента её списка
@@ -43,7 +43,7 @@ class NewVisitorTest(FunctionalTest):
     def test_multiple_users_can_start_lists_at_different_urls(self):
         # Эдит начинает новый список
         self.browser.get(self.live_server_url)
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box()
         inputbox.send_keys("Купить павлиньи перья")
         inputbox.send_keys(Keys.ENTER)
 
@@ -68,7 +68,7 @@ class NewVisitorTest(FunctionalTest):
 
         # Фрэнсис начинает новый список, вводя новый элемент. Он менее
         # интересен, чем список Эдит
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box()
         inputbox.send_keys('Купить молоко')
         inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table("1: Купить молоко")
