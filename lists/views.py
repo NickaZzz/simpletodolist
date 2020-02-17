82f87d40db83799e180fd9ff31e50308139f6ba1
Azanov Nikita <azanov.nik74@gmail.com>
2020-02-17 23:29:57 +0700

    Add URL, view and templates for my_lists

--- lists/views.py
+++ lists/views.py
@@ -29,3 +29,7 @@ def new_list(request):
         return redirect(list_)
     else:
         return render(request, 'home.html', {"form": form})
+
+
+def my_lists(request, user):
+    return render(request, 'my_lists.html')
