aee8ec2fd706ac53cb29ca7b0d8d4a325bf7fc54
Azanov Nikita <azanov.nik74@gmail.com>
2020-02-17 23:42:39 +0700

    Try to assign the owner to my list, but not be able to

--- lists/views.py
+++ lists/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, redirect
 from django.core.exceptions import ValidationError
 from django.http import HttpResponse
+from django.contrib.auth import get_user_model
 
 from lists.models import Item, List
 from lists.forms import ItemForm, ExistingListItemForm
+
+User = get_user_model()
 
 
 def home_page(request):
@@ -24,7 +27,9 @@ def view_list(request, list_id):
 def new_list(request):
     form = ItemForm(data=request.POST)
     if form.is_valid():
-        list_ = List.objects.create()
+        list_ = List()
+        list_.owner = request.user
+        list_.save()
         form.save(for_list=list_)
         return redirect(list_)
     else:
@@ -32,4 +37,5 @@ def new_list(request):
 
 
 def my_lists(request, user):
-    return render(request, 'my_lists.html')
+    owner = User.objects.get(email=user)
+    return render(request, 'my_lists.html', {'owner': owner})
